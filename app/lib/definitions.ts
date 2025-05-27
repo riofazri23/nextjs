ef796b03b463e97bbc1b5da1f532d147292f7885
Rio Fazri Septian <rfazri7@gmail.com>
2025-05-27 10:59:58 +0700

    initial comit

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,21 +1,3 @@
-// This file contains type definitions for your data.
-// It describes the shape of the data, and what data type each property should accept.
-// For simplicity of teaching, we're manually defining these types.
-// However, these types are generated automatically if you're using an ORM such as Prisma.
-export type User = {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-};
-
-export type Customer = {
-  id: string;
-  name: string;
-  email: string;
-  image_url: string;
-};
-
 export type Invoice = {
   id: string;
   customer_id: string;
@@ -25,64 +7,45 @@ export type Invoice = {
   // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
   status: 'pending' | 'paid';
 };
-
-export type Revenue = {
-  month: string;
-  revenue: number;
-};
-
-export type LatestInvoice = {
+export type Customer = {
   id: string;
   name: string;
-  image_url: string;
   email: string;
-  amount: string;
+  image_url: string | null; // image_url can be null if no image is provided
 };
-
-// The database returns a number for amount, but we later format it to a string with the formatCurrency function
-export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
-  amount: number;
-};
-
-export type InvoicesTable = {
-  id: string;
-  customer_id: string;
-  name: string;
-  email: string;
-  image_url: string;
-  date: string;
-  amount: number;
-  status: 'pending' | 'paid';
-};
+export type CustomerField = keyof Customer;
 
 export type CustomersTableType = {
   id: string;
   name: string;
   email: string;
-  image_url: string;
+  image_url: string | null;
   total_invoices: number;
-  total_pending: number;
   total_paid: number;
-};
-
-export type FormattedCustomersTable = {
-  id: string;
-  name: string;
-  email: string;
-  image_url: string;
-  total_invoices: number;
-  total_pending: string;
-  total_paid: string;
-};
-
-export type CustomerField = {
-  id: string;
-  name: string;
+  total_pending: number;
 };
 
 export type InvoiceForm = {
-  id: string;
   customer_id: string;
   amount: number;
+  date: string;
   status: 'pending' | 'paid';
+};
+
+export type InvoicesTable = Invoice & {
+  customer_name: string;
+  customer_email: string;
+};
+
+export type LatestInvoiceRaw = {
+  id: string;
+  amount: number;
+  date: string;
+  customer_name: string;
+  customer_email: string;
+};
+
+export type Revenue = {
+  month: string;
+  revenue: number;
 };
